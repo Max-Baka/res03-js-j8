b8627375933b2b4bddcad5e5cbab3f4866394078
Max-Baka <maximelanglois@ip-172-31-24-38.eu-west-3.compute.internal>
2023-01-12 09:56:34 +0000

    projet

---
+++ prj/assets/js/index.js
@@ -0,0 +1,19 @@
+import { Ingredients } from './ingredients.js';
+window.addEventListener("DOMContentLoaded", function(){
+    
+    let ingredients = new Bacon("assets/img/bacon.png");
+    let ingredients = new Carotte("assets/img/carrots.png");
+    let ingredients = new Fromage("assets/img/cheese.png");
+    let ingredients = new Oeuf("assets/img/egg.png");
+    let ingredients = new Aubergine("assets/img/eggplant.png");
+    let ingredients = new Fromage de chèvre("assets/img/goat-cheese.png");
+    let ingredients = new Miel("assets/img/honey.png");
+    let ingredients = new Champignon("assets/img/mushroom.png");
+    let ingredients = new Olive("assets/img/olive.png");
+    let ingredients = new Piment("assets/img/pepper.png");
+    let ingredients = new Pomme de terre("assets/img/potato.png");
+    let ingredients = new Tomate("assets/img/tomato.png");
+    
+
+    console.log(character);
+});
